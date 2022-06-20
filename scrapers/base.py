d69696330d3e76af0dfe87a0a833794ec42feb9b
Reshav Abraham <reshavabraham@gmail.com>
2022-06-20 16:39:50 -0400

    scraper desc

--- scrapers/base.py
+++ scrapers/base.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 
 class BaseScraper:
+    # A class for scraping html. Will eventually have some light nlp 
+    # functions as well
     def get_html_data(self, url):
         response = requests.get(url)
         if response.status_code == 200:
