51f29e0bd2030dd062204a70fa9f930ec911ac34
“Reshav” <reshav@narmitech.com>
2023-07-23 12:16:13 -0400

    stuff

--- main.py
+++ main.py
@@ -10,17 +10,16 @@ app = FastAPI()
 
 @app.get("/callback")
 def read_root(code):
-    print(code)
-    # code = request.GET.get("code", '')
     spotify_client.get_spotify_access_token(code)
     return RedirectResponse("http://localhost:8888")
 
 @app.get("/login")
 def login():
     result = spotify_client.get_login_redirect()
-    print(result)
     return RedirectResponse(result)
 
 @app.get("/")
 def read_item():
+    # return user html page 
+    # src javascript file
     return spotify_client.get_me()
