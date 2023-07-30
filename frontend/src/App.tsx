04e834cb63a637784f3fd4a95a6623dadc99b054
“Reshav” <reshav@narmitech.com>
2023-07-30 16:47:50 -0400

    roots

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
+import Upload from './Upload';
 
 function App() {
+  console.log("app!");
   return (
-    <div className="dashboard">
+    <>
+      <div className="dashboard">
         <p>a component</p>
-    </div>
+      </div>
+      <div id="upload"></div>
+      <Upload></Upload>
+    </>
   );
 }
 
 export default App;
+
---
+++ frontend/src/Upload.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import ReactDOM from "react-dom/client";
+
+function Upload() {
+
+    console.log("upload");
+
+    return (
+        <div className="dashboard">
+            <p>a component 2</p>
+        </div>
+    );
+}
+
+// const rootElement = document.getElementById("upload");
+
+// const domNode = document.getElementById('upload');
+// if (!domNode) {
+//     console.log("no upload id"); 
+// } else {
+//     const root = createRoot(domNode);
+//     // root.render(<Upload />); 
+//     root.render(
+//         <React.StrictMode>
+//           <Upload />
+//         </React.StrictMode>
+//       );
+// }
+// const root = createRoot(document.getElementById('upload'));
+// root.render(
+//   <React.StrictMode>
+//     <Upload />
+//   </React.StrictMode>
+// );
+
+
+let container = document.getElementById('upload')
+// if(container) (container as HTMLFormElement).reset(); 
+if (container) {
+    const root = ReactDOM.createRoot(container);
+
+    root.render(
+        <React.StrictMode>
+            <Upload />
+        </React.StrictMode>,
+    );
+
+}
+// if(container){
+//     const root = ReactDOM.createRoot(container); 
+
+//     root.render(
+//       <React.StrictMode>
+//           <Upload />
+//       </React.StrictMode>,
+//     );
+// } else {
+//     console.log("root not found")
+// }
+
+export default Upload
